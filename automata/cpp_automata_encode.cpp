4f9a645a9f6a9b42a56dbd9db0c6956ee9c9f6c0
gould25 <jg7f9@msy.edu>
2017-02-01 23:17:42 -0600

    c++ file running correctly

--- automata/cpp_automata_encode.cpp
+++ automata/cpp_automata_encode.cpp
@@ -9,6 +9,7 @@ Description: C++ program for encoding an automata
 
 #include <iostream>
 #include <string>
+#include <string.h>
 
 using namespace std;
 
@@ -21,7 +22,7 @@ bool recognize(string s);
 
 string s;
 char retry;
-bool check = false;
+bool check = true;
 
 // end Declarations
 
@@ -29,21 +30,73 @@ int main() {
 
   do {
 
-    cout << "Enter a string from the alphabet (a,b)" << endl;
+    cout << "Enter a string from the alphabet (a,b) ";
     cin >> s;
+    cout << endl;
 
-    //check = recognize(s);
+    check = recognize(s);
 
     if (check){
-      cout << "YAY String " << s << " is in the alphabet (a,b)!!!!!" << endl;
+      cout << "YAY String " << s << " is in the alphabet (a,b)!!!!!";
+      cout << endl << endl;
     }
     else {
-      cout << "Sorry String " << s << "isn't in the alphabet (a,b)" << endl;
+      cout << "Sorry String " << s << " isn't in the alphabet (a,b)";
+      cout << endl << endl;
     }
 
     cout << "Do you want to check another sting? ";
     cin >> retry;
+    cout << endl << endl;
 
-  } while (retry != 'y' || retry != 'Y');
+  } while (retry == 'y');
 
+  cout << "Thanks have a great day!!!!!";
+
+  return 0;
 }
+
+bool recognize(string s) {
+  int state = 1;
+  int i = 0;
+
+  while ( i < s.length()) {
+    switch (state) {
+
+      case 1: if ( s[i] == 'a' )
+                state = 2;
+              else
+                state = 1;
+              break;
+
+      case 2: if ( s[i] == 'a' )
+                state = -1; // failure
+              else
+                state = 3;
+              break;
+
+      case 3: if ( s[i] == 'a' )
+                state = 2; // failure
+              else
+                state = 4;
+              break;
+
+      case 4: if ( s[i] == 'a' )
+                state = 4; // failure
+              else
+                state = 2;
+              break;
+
+      default:
+              i = s.length();
+              break;
+    } // end switch
+
+    i ++;
+  } // end while
+
+  if ( i == s.length() && state == 4 )
+    return true;
+
+  return false;
+} // end fnct
