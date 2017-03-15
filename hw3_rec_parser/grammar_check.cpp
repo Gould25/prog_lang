7fc646bfe27a97d247585f9605a8c513bba26756
gould25 <jg7f9@msy.edu>
2017-03-15 02:01:15 -0500

    through expression

--- hw3_rec_parser/grammar_check.cpp
+++ hw3_rec_parser/grammar_check.cpp
@@ -13,12 +13,23 @@ string get_token();
 // function Name: is_factor(token)
 // Purpose: to find if the string is a factor
 // Postcondition: returns true or false
-bool is_factor(string token);
+bool is_factor();
 
-// function Name: is_term(token)
+// function Name: is_term()
 // Purpose: to find if the string is a term
 // Postcondition: returns true or false
-bool is_term(string token);
+bool is_term();
+
+// function Name: is_simple_exp()
+// Purpose: to find if the string is a simple expression
+// Postcondition: returns true or false
+bool is_simple_exp();
+
+// function Name: is_simple_exp()
+// Purpose: to find if the string is a simple expression
+// Postcondition: returns true or false
+bool is_expression();
+
 
 string token;
 
@@ -27,17 +38,21 @@ int main() {
 
   /*****     Declarations     *****/
 
+int i = 0;
 
   /*****     Main Body     *****/
-
   get_token();
-  while (token != "$"){
 
-    if (is_term(token))
-      cout << token << " TRUE" << endl; ;
-    cout << token << " FALSE" << endl;;
+  //do {
 
-  }//end while
+    if (is_expression())
+      cout << token << "\tCORRECT   " << i << endl;
+    else{
+      cout << token << "\tINVALID!   " << i << endl;
+    }
+  //  get_token();
+    i++;
+  //}while(token != "$");
 
   return 0;
 }//end main
@@ -45,13 +60,17 @@ int main() {
 /*****     Get Token Function     *****/
 
 string get_token(){
-  cin >> token;
-  return token;
+  if(cin >> token)
+    return token;
+  else{
+    token = "$";
+    return token;
+  }
 }
 
 /*****     Factor Function     *****/
 
-bool is_factor(string token){
+bool is_factor(){
   if ( is_integer(token) | is_decimal(token) |
         is_identifier(token) | is_string(token) )
       return true;
@@ -60,23 +79,136 @@ bool is_factor(string token){
 
 /*****     Term Function     *****/
 
-bool is_term(string token){
+bool is_term(){
 
-  if ( is_factor(token) ){
-    cout << "before call " << token << endl;
-    token = get_token();
-    cout << "after call " << token << endl;
-    if (is_mulop(token)){
-      cout << "before mulop call " << token << endl;
-      token = get_token();
-      cout << "after mulop call " << token << endl;
-      is_term(token);
-    }
-    else if (token == "!"){
+  bool check;
+
+  if ( is_factor() ) {
+    cerr << "IN TERM before token call " << token << endl;
+
+    get_token();
+    cerr << "IN TERM after token call " << token << endl;
+
+    check = true;
+  }
+  else
+    return false;
+
+    while ( is_mulop(token) ){
+        get_token();
+        if(is_factor()){
+            check = true;
+            get_token();
+            //if ( is_mulop(token) || token == "$")
+            //  check = true;
+        } // end if
+        else
+          return false;
+    } // end while
+
+  /*****    RETURN SECTION     ******/
+
+  if (check ){
+    cerr << "TERM ret truye " << token << endl;
+    return true;
+  }
+  else {
+    return false;
+  }
+  //get_token();
+
+} // end is_term
+
+/*****     Simple Expression Function     *****/
+
+bool is_simple_exp(){
+
+  bool check;
+
+  if ( is_term() ) {
+    //get_token();
+    cerr << "In Simple " << token << endl;
+
+    check = true;
+  }
+  else
+  cerr << "In Simple FAIIIIIIIIIIIIIIIIILLLLLLL   " << token << endl; //return false;
+  if ( is_addop(token) ) {
+    cerr << "In Simple  addop before while  " << token << endl;
+
+    while ( is_addop(token) ){
       get_token();
-      return true;
+      if(is_term()){
+        check = true;
+        if ( is_addop(token) || token == "$" || token == "]")
+          check = true;
+        else
+          return false;
+      } // end if
+      else
+        return false;
+    } // end while
+  } // end if
+  //else
+    //return false;
+
+ /*****    RETURN SECTION     ******/
+
+  if (check){
+    cerr << "Simple Ret Treue " << token << endl;
+
+    return true;
+  }
+  else {
+    return false;
+  }
+  //get_token();
+
+} // end is_simple_exp
+
+/*****     Expression Function     *****/
+
+bool is_expression(){
+
+  bool check;
+
+  if ( is_simple_exp() ) {
+    //get_token();
+    check = true;
+  }
+  else
+    check = false;
+
+  if (token == "["){
+    cerr << "Begin Bracket before " << token << endl;
+    get_token();
+    if (is_relation(token)){
+      cerr << "Relation before " << token << endl;
+      get_token();
+      cerr << "Relation after " << token << endl;
+      if (is_simple_exp()){
+        //get_token();
+        cerr << "Simple " << token << endl;
+        if (token == "]"){
+          return true;
+        }
+        else
+          check = false;
+      }
+      else
+        check = false;
     }
     else
-      return false;
-  } // eif factor*/
-} // end is_term
+      check = false;
+  }
+ /*****    RETURN SECTION     ******/
+
+  if (check){
+    return true;
+  }
+  else {
+    return false;
+  }
+  get_token();
+
+} // end expression
